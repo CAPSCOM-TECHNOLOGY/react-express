812851c795c155be4b1cd8b7191c7de2b3e5a70a
Hemant singh <capscomtech@gmail.com>
2021-01-17 23:04:07 +0530

    final

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+const axios=require("axios")
+function App(){
+  let [users,setusers]=useState([])
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const loaduser=()=>{
+        axios.get("http://localhost:5000/userslist")
+        .then(function(res){
+          console.log(res)
+        })
+        .catch(function(err){
+          console.log(err)
+        })
+  }
+
+  return(
+    <>
+      <button type="button" onClick={loaduser}>Load user</button>
+    </>
+  )
 }
-
-export default App;
+export default App 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/userslist",function(req,res,next){
+  let users=[
+    {"name":"aman","age":20,"address":"almora"},
+    {"name":"rakesh","age":20,"address":"almora"},
+    {"name":"ajay","age":20,"address":"almora"},
+    {"name":"ankit","age":20,"address":"almora"},
+    {"name":"ramesh","age":20,"address":"almora"},
+  ]
+  res.json(users)
+})
+
 module.exports = router;
